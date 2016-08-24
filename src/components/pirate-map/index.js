103e18b985ec54f2b9d869ab07ebffc62c9476ed
C. Prinse <christjan.prinse@logius.nl>
2016-08-24 11:07:07 +0200

    remove coding crap

--- src/components/pirate-map/index.js
+++ src/components/pirate-map/index.js
@@ -1,15 +1,4 @@
 import Vue from 'vue';
 import {load, Map, Marker} from 'vue-google-maps';
 
-export default Vue.extend(Map, {
-    data() {
-        return {
-            center:{lat: 1.38, lng: 103.8},
-            zoom:"12"
-        }
-    },
-    created: function() {
-        console.log('pirate map')
-    }
-
-})
+export default Vue.extend(Map, {})
