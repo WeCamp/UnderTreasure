cab8cc89bb09a0371318242a657786318f66f600
Dennis Vergeer <dennis@seats2meet.com>
2016-08-26 11:14:44 +0200

    Highscore entity
    Updated entity with get set methods

--- underTreasure/src/AppBundle/Entity/Highscore.php
+++ underTreasure/src/AppBundle/Entity/Highscore.php
@@ -11,8 +11,39 @@ namespace AppBundle\Entity;
 
 class Highscore
 {
+	/**
+	 * @var integer
+	 */
 	private $id;
+
+	/**
+	 * @var string
+	 */
 	private $username;
+
+	/**
+	 * @var integer
+	 */
 	private $time;
+
+	/**
+	 * @var integer
+	 */
 	private $coins;
+
+	/**
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * @param int $id
+	 */
+	public function setId($id)
+	{
+		$this->id = $id;
+	}
 }
