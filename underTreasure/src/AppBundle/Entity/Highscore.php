280a718061c9798f7f70179604d1ff00f48052e4
Dennis Vergeer <dennis@seats2meet.com>
2016-08-26 11:08:02 +0200

    Added backend
    - Created database table entity
    - Setting configuration

---
+++ underTreasure/src/AppBundle/Entity/Highscore.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dennis
+ * Date: 26-8-2016
+ * Time: 10:55
+ */
+
+namespace AppBundle\Entity;
+
+
+class Highscore
+{
+	private $id;
+	private $username;
+	private $time;
+	private $coins;
+}
